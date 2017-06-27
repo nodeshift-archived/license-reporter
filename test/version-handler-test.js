ccaedad32324a7417686f36b16e44c49c66808c4
Helio Frota <00hf11@gmail.com>
2017-06-27 09:56:56 -0300

    src: one more test added.

--- test/version-handler-test.js
+++ test/version-handler-test.js
@@ -17,3 +17,12 @@ test('Should fail matching npm version.', (t) => {
   t.equal(null, result, 'the version not match.');
   t.end();
 });
+
+test('Should return as npm version.', (t) => {
+  t.plan(2);
+  const npmVersion = versionHandler.asNpmVersion('foo', '3.2.1');
+  const result = versionHandler.fromNpmVersion(npmVersion);
+  t.equal('foo', result.name, 'the name is OK.');
+  t.equal('3.2.1', result.version, 'the version is OK.');
+  t.end();
+});
