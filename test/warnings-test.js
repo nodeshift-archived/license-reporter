02f682fc78d3ca84db0603fb9804b35dd8c22f0f
Felipe Martins <felipewmartins@gmail.com>
2017-07-06 23:38:29 -0300

    add warnings to the new file #32
    The warnings is now in the separated module, out of the cli.js

--- test/warnings-test.js
+++ test/warnings-test.js
@@ -5,21 +5,21 @@ const warnings = require('../lib/warnings.js');
 const stdout = require('test-console').stdout;
 
 test('Should report the correct warning.', (t) => {
-    t.plan(1);
-    const project = {
+  t.plan(1);
+  const project = {
     name: 'testProject',
     licenses: {
       license: [
-        {name: 'test1', version: '1.0', license: 'MIT', file: '...'},
-        {name: 'test2', version: '1.2', license: 'Bogus', file: '...'},
-        {name: 'test2', version: '1.2', license: 'ASL 1.1', file: '...'}
+       {name: 'test1', version: '1.0', license: 'MIT', file: '...'},
+       {name: 'test2', version: '1.2', license: 'Bogus', file: '...'},
+       {name: 'test2', version: '1.2', license: 'ASL 1.1', file: '...'}
       ]
     }
   };
 
-  const whitelist = [{'name': 'MIT'}];
+  const whitelist = [{'name': 'ASL 1.1'}];
   const log = stdout.inspectSync(() => warnings.printWarning(require('../lib/whitelist.js')(whitelist).check(project),
-                   'WHITE-LISTED'));
-  t.deepEqual(log, [project.licenses.license[0]], 'WHITE');                
+                 'WHITE-LISTED'));
+  t.deepEqual(log, ['name: test2 , version: 1.2 , licenses: Bogus\n'], 'WHITE-LISTED');
   t.end();
 });
