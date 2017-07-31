9468fc175cfb95cf77c47ddf492437b226971d3b
Helio Frota <00hf11@gmail.com>
2017-07-31 13:57:01 -0300

    src: display warning when no node_modules found Also removing unnecessary console.log

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,7 @@ const warnings = require('../lib/warnings.js');
 const reader = require('../lib/file-reader.js');
 
 const fs = require('fs');
+const path = require('path');
 
 function entry (info, npmVersion) {
   var entry = {
@@ -82,35 +83,50 @@ function checkLicense (options) {
   });
 }
 
-function run (options) {
-  if (options.merge) {
-    if (!options.mergeProductName) {
-      console.error('merge feature requires a product name');
-      process.exit(1);
-    }
-    if (!options.mergeXmls) {
-      console.error('merge feature requires a two or more licence.xml files to merge');
-      process.exit(1);
-    }
-    const xmls = [];
-    options.mergeXmls.split(',').forEach((file) => {
-      xmls.push(fs.readFileSync(file.trim(), 'utf8'));
-    });
-    xml.merge(options.mergeProductName, xmls).then(result => {
-      if (!options.silent) {
-        console.log(result);
-      }
-      if (options.mergeOutput) {
-        fs.writeFileSync(options.mergeOutput, result);
-      }
-    }).catch(e => {
-      console.error(e);
-      process.exit(2);
-    });
-    return;
-  }
+/*
+* Checks if node_modules exists and returns
+* true if it is not empty not considering '.bin'
+* directory.
+*/
+function nodeModulesFound (dir) {
+  const modulesDir = path.join(dir, 'node_modules');
+  const content = fs.readdirSync(modulesDir).filter(e => e !== '.bin');
+  return fs.existsSync(modulesDir) && content.length > 0;
+}
 
-  checkLicense(options);
+function run (options) {
+  if (nodeModulesFound(options.directory)) {
+    if (options.merge) {
+      if (!options.mergeProductName) {
+        console.error('merge feature requires a product name');
+        process.exit(1);
+      }
+      if (!options.mergeXmls) {
+        console.error('merge feature requires a two or more licence.xml files to merge');
+        process.exit(1);
+      }
+      const xmls = [];
+      options.mergeXmls.split(',').forEach((file) => {
+        xmls.push(fs.readFileSync(file.trim(), 'utf8'));
+      });
+      xml.merge(options.mergeProductName, xmls).then(result => {
+        if (!options.silent) {
+          console.log(result);
+        }
+        if (options.mergeOutput) {
+          fs.writeFileSync(options.mergeOutput, result);
+        }
+      }).catch(e => {
+        console.error(e);
+        process.exit(2);
+      });
+      return;
+    }
+
+    checkLicense(options);
+  } else {
+    console.error('You need to run \'npm install\' before search for license information.');
+  }
 }
 
 module.exports = {
