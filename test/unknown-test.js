abea87bab24f3cd2ed3b06282affb189f3ca2aa7
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-08-22 14:42:10 +0200

    src: mark custom license as UNKNOWN
    We have gotten feedback on the output of a custom license which simply
    contains a link to the project in question. The request was to mark
    such licenses as unknown instead.

--- test/unknown-test.js
+++ test/unknown-test.js
@@ -15,11 +15,13 @@ test('Should warn if license is unknown', (t) => {
         {name: 'test5', version: '1.2', license: undefined, file: undefined},
         {name: 'test6', version: '1.0', license: ['MIT', 'APACHE'], file: 'something'},
         {name: 'test7', version: '1.0', license: ['unknown'], file: 'something'},
-        {name: 'test8', version: '1.0', license: ['MIT', 'unknown'], file: 'something'}
+        {name: 'test8', version: '1.0', license: ['MIT', 'unknown'], file: 'something'},
+        {name: 'test9', version: '1.0', license: 'Custom: https://something', file: 'something'},
+        {name: 'test10', version: '1.0', license: ['Custom: https://something'], file: 'something'}
       ]
     }
   };
   const unknown = require('../lib/unknown.js').check(project);
-  t.equal(unknown.length, 6);
+  t.equal(unknown.length, 8);
   t.end();
 });
