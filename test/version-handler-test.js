a44c0144563987dbd0c57d36bba846f1ead388ed
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-08-28 08:48:52 +0200

    src: support version with dash postfix
    Currently, if a dependency has a version with a postfix version number,
    for example ups-admin-ui has the version 1.1.4-10 the `-10` was not
    taken into account when determining the version.
    
    This commit fixes the above issue and also fixes the issue that this
    version was included in both the unknown list (as it does not have a
    license that was detected) and in the whitelist. The issue here was that
    the while list only inspected the actual license name and ignored the
    license file. It now checks the license file and if it is unknown does
    not report it as whitelisted, instead the unknown list will report this
    license.
    
    Fixes: https://github.com/bucharest-gold/license-reporter/issues/99

--- test/version-handler-test.js
+++ test/version-handler-test.js
@@ -14,15 +14,36 @@ test('Should succeed matching npm version.', (t) => {
 test('Should fail matching npm version.', (t) => {
   t.plan(1);
   const result = versionHandler.fromNpmVersion('FooBar');
-  t.equal(null, result, 'the version not match.');
+  t.equal(undefined, result.version, 'the version not match.');
   t.end();
 });
 
 test('Should return as npm version.', (t) => {
   t.plan(2);
-  const npmVersion = versionHandler.asNpmVersion('foo', '3.2.1');
-  const result = versionHandler.fromNpmVersion(npmVersion);
+  const result = npmVersion('foo', '3.2.1');
   t.equal('foo', result.name, 'the name is OK.');
   t.equal('3.2.1', result.version, 'the version is OK.');
   t.end();
 });
+
+test('Should comply with semver spec.', (t) => {
+  t.plan(4);
+  t.equal('3.20.1-14', npmVersion('foo', '3.20.1-14').version);
+  t.equal('1.0.0-x.7.z.92', npmVersion('foo', '1.0.0-x.7.z.92').version);
+  t.equal('1.0.0-alpha+001', npmVersion('foo', '1.0.0-alpha+001').version);
+  t.equal('1.0.0+20130313144700', npmVersion('foo', '1.0.0+20130313144700').version);
+  t.end();
+});
+
+test('Should work with scoped package naming', (t) => {
+  t.plan(2);
+  const result = npmVersion('@org/foo', '1.0.0+20130313144700');
+  t.equal('@org/foo', result.name);
+  t.equal('1.0.0+20130313144700', result.version);
+  t.end();
+});
+
+function npmVersion (name, version) {
+  const npmVersion = versionHandler.asNpmVersion(name, version);
+  return versionHandler.fromNpmVersion(npmVersion);
+}
