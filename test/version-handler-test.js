5a9a8700f5a76897ee380bd03ab73d6aa1eb2564
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-08-28 12:37:46 +0200

    remove reges and use simple split for name/version

--- test/version-handler-test.js
+++ test/version-handler-test.js
@@ -14,23 +14,28 @@ test('Should succeed matching npm version.', (t) => {
 test('Should fail matching npm version.', (t) => {
   t.plan(1);
   const result = versionHandler.fromNpmVersion('FooBar');
-  t.equal(null, result, 'the version not match.');
+  t.equal(undefined, result.version, 'the version not match.');
   t.end();
 });
 
 test('Should return as npm version.', (t) => {
   t.plan(2);
-  const npmVersion = versionHandler.asNpmVersion('foo', '3.2.1');
-  const result = versionHandler.fromNpmVersion(npmVersion);
+  const result = npmVersion('foo', '3.2.1');
   t.equal('foo', result.name, 'the name is OK.');
   t.equal('3.2.1', result.version, 'the version is OK.');
   t.end();
 });
 
-test('Should return as npm version with postfix.', (t) => {
-  t.plan(1);
-  const npmVersion = versionHandler.asNpmVersion('foo', '3.2.1-14');
-  const result = versionHandler.fromNpmVersion(npmVersion);
-  t.equal('3.2.1-14', result.version, 'the version is OK.');
+test('Should comply with semver spec.', (t) => {
+  t.plan(4);
+  t.equal('3.20.1-14', npmVersion('foo', '3.20.1-14').version);
+  t.equal('1.0.0-x.7.z.92', npmVersion('foo', '1.0.0-x.7.z.92').version);
+  t.equal('1.0.0-alpha+001', npmVersion('foo', '1.0.0-alpha+001').version);
+  t.equal('1.0.0+20130313144700', npmVersion('foo', '1.0.0+20130313144700').version);
   t.end();
 });
+
+function npmVersion (name, version) {
+  const npmVersion = versionHandler.asNpmVersion(name, version);
+  return versionHandler.fromNpmVersion(npmVersion);
+}
