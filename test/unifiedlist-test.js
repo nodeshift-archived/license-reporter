af98203d756c873ecbdc3a6bc77364153a499365
helio-frota <00hf11@gmail.com>
2017-09-05 14:14:10 -0300

    src: Improve unit test for unifiedlist

--- test/unifiedlist-test.js
+++ test/unifiedlist-test.js
@@ -2,41 +2,122 @@
 
 const test = require('tape');
 const stdout = require('test-console').stdout;
+const unifiedlist = require('../lib/unifiedlist.js');
 
-test('Should warn if license is not not approved', (t) => {
+const xmlObject = {
+  dependencies: {
+    dependency: [
+      {
+        packageName: 'testProject',
+        version: '1.0.0',
+        licenses: {
+          license: [
+            {name: 'MIT', url: '...'}
+          ]
+        }
+      },
+      {
+        packageName: 'notApproved',
+        version: '2.0.0',
+        licenses: {
+          license: [
+            {name: '9wm License (Original)', url: '...'}
+          ]
+        }
+      }
+    ]
+  }
+};
+
+test('Should get the licenses from xmlObject', (t) => {
+  t.plan(3);
+  const licenses = unifiedlist._getLicensesFromXmlObject(xmlObject);
+  t.equal(licenses.length, 2);
+  t.equal(licenses[0].license, 'MIT');
+  t.equal(licenses[1].license, '9wm License (Original)');
+  t.end();
+});
+
+test('Should get approved only from xmlObject based on unified list', (t) => {
+  t.plan(1);
+  const licenses = unifiedlist._getLicensesFromXmlObject(xmlObject);
+  const approvedList = [];
+  const unifiedList = require('../lib/resources/default-unifiedlist.json');
+  Object.keys(unifiedList).forEach(key => {
+    if (unifiedList[key].approved === 'yes') {
+      approvedList.push(unifiedList[key]);
+    }
+  });
+  const approved = unifiedlist._findApproved(approvedList, licenses);
+  t.equal(Array.from(approved)[0].license, 'MIT');
+  t.end();
+});
+
+test('Should get not approved only from xmlObject based on unified list', (t) => {
+  t.plan(1);
+  const licenses = unifiedlist._getLicensesFromXmlObject(xmlObject);
+  const notApprovedList = [];
+  const unifiedList = require('../lib/resources/default-unifiedlist.json');
+  Object.keys(unifiedList).forEach(key => {
+    if (unifiedList[key].approved !== 'yes') {
+      notApprovedList.push(unifiedList[key]);
+    }
+  });
+  const notApproved = unifiedlist._findNotApproved(notApprovedList, licenses);
+  t.equal(Array.from(notApproved)[0].license, '9wm License (Original)');
+  t.end();
+});
+
+test('Should print approved licenses', (t) => {
+  t.plan(1);
+  const expected = ['========= APPROVED LICENSES        ==========\n',
+    'name: testProject , version: 1.0.0 , licenses: MIT\n',
+    '========= APPROVED LICENSES        ==========\n'];
+
+  const licenses = unifiedlist._getLicensesFromXmlObject(xmlObject);
+  const approvedList = [];
+  const unifiedList = require('../lib/resources/default-unifiedlist.json');
+  Object.keys(unifiedList).forEach(key => {
+    if (unifiedList[key].approved === 'yes') {
+      approvedList.push(unifiedList[key]);
+    }
+  });
+  const approved = unifiedlist._findApproved(approvedList, licenses);
+  const log = stdout.inspectSync(() => { unifiedlist._printApproved(approved); });
+  t.deepEqual(log, expected);
+  t.end();
+});
+
+test('Should print not approved licenses', (t) => {
+  t.plan(1);
+  const expected = ['========= NOT APPROVED LICENSES    ==========\n',
+    'name: notApproved , version: 2.0.0 , licenses: 9wm License (Original)\n',
+    '========= NOT APPROVED LICENSES    ==========\n'];
+
+  const licenses = unifiedlist._getLicensesFromXmlObject(xmlObject);
+  const notApprovedList = [];
+  const unifiedList = require('../lib/resources/default-unifiedlist.json');
+  Object.keys(unifiedList).forEach(key => {
+    if (unifiedList[key].approved !== 'yes') {
+      notApprovedList.push(unifiedList[key]);
+    }
+  });
+  const notApproved = unifiedlist._findNotApproved(notApprovedList, licenses);
+  const log = stdout.inspectSync(() => { unifiedlist._printNotApproved(notApproved); });
+  t.deepEqual(log, expected);
+  t.end();
+});
+
+test('Should print approved and approved licenses', (t) => {
   t.plan(1);
   const expected = ['========= APPROVED LICENSES        ==========\n',
     'name: testProject , version: 1.0.0 , licenses: MIT\n',
     '========= APPROVED LICENSES        ==========\n',
     '========= NOT APPROVED LICENSES    ==========\n',
-    'name: notapproved , version: 2.0.0 , licenses: 9wm License (Original)\n',
+    'name: notApproved , version: 2.0.0 , licenses: 9wm License (Original)\n',
     '========= NOT APPROVED LICENSES    ==========\n'];
-  const project = {
-    dependencies: {
-      dependency: [
-        {
-          packageName: 'testProject',
-          version: '1.0.0',
-          licenses: {
-            license: [
-              {name: 'MIT', url: '...'}
-            ]
-          }
-        },
-        {
-          packageName: 'notapproved',
-          version: '2.0.0',
-          licenses: {
-            license: [
-              {name: '9wm License (Original)', url: '...'}
-            ]
-          }
-        }
-      ]
-    }
-  };
   const log = stdout.inspectSync(() => {
-    require('../lib/unifiedlist.js').check(project);
+    unifiedlist.check(xmlObject);
   });
   t.deepEqual(log, expected);
   t.end();
