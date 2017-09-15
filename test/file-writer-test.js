2c3ee9a78fc943e908f44ed8dd2699cdef4f92ea
Helio Frota <00hf11@gmail.com>
2017-09-15 17:51:35 -0300

    src: tests to create XML and HTML

---
+++ test/file-writer-test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const test = require('tape');
+const fs = require('fs');
+const path = require('path');
+const writer = require('../lib/file-writer.js');
+
+const xmlObject = {
+  dependencies: {
+    dependency: [
+      {
+        packageName: 'testProject',
+        version: '1.0.0',
+        licenses: {
+          license: [
+            {name: 'MIT', url: '...'}
+          ]
+        }
+      },
+      {
+        packageName: 'notApproved',
+        version: '2.0.0',
+        licenses: {
+          license: [
+            {name: '9wm License (Original)', url: '...'}
+          ]
+        }
+      }
+    ]
+  }
+};
+
+test('Should create foo.xml.', (t) => {
+  writer.createXml({file: 'foo.xml', silent: true}, xmlObject);
+  t.equal(fs.existsSync('foo.xml'), true, 'foo.xml file created.');
+  fs.unlinkSync('foo.xml');
+  t.end();
+});
+
+test('Should create HTML file.', (t) => {
+  const options = {css: path.join(__dirname, '../lib/resources/licenses.css')};
+  options.directory = process.cwd();
+  Promise.resolve(writer.createHtml(options, xmlObject))
+  .then(() => {
+    t.equal(fs.existsSync('license.html'), true, 'HTML file created.');
+    fs.unlinkSync('license.html');
+    fs.unlinkSync('licenses.css');
+    t.end();
+  });
+});
