76b362575ff9c607c9350bcb9bcc8869ad5e985f
helio-frota <00hf11@gmail.com>
2017-09-18 14:07:14 -0300

    src: trying a more stable build
    * src: trying a more stable build

--- test/file-writer-test.js
+++ test/file-writer-test.js
@@ -43,11 +43,7 @@ test('Should create HTML file.', (t) => {
   return Promise.resolve(writer.createHtml(options, xmlObject))
   .then(() => {
     t.equal(fs.existsSync('license.html'), true, 'HTML file created.');
+    fs.unlink('license.html', () => {});
+    fs.unlink('licenses.css', () => {});
   });
-});
-
-test('Should cleanup.', (t) => {
-  fs.unlinkSync('license.html');
-  fs.unlinkSync('licenses.css');
-  t.end();
 });
