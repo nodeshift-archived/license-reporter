8f9bfc5731091a64b01ac8d2d9b7ff9c3a7a0c9c
helio-frota <00hf11@gmail.com>
2017-10-26 11:38:08 -0300

    src: more refactor

--- test/file-writer-test.js
+++ test/file-writer-test.js
@@ -3,9 +3,9 @@
 const test = require('blue-tape');
 const fs = require('fs');
 const path = require('path');
-const writer = require('../lib/file-writer.js');
+const writer = require('../lib/file-writer');
+const util = require('../lib/util');
 
-const licensesDir = path.join(process.cwd(), 'licenses');
 writer.createLicenseDir();
 
 const xmlObject = {
@@ -35,14 +35,14 @@ const xmlObject = {
 
 test('Should create foo.xml.', (t) => {
   writer.createXml({xml: 'foo.xml', silent: true}, xmlObject);
-  t.equal(fs.existsSync(path.join(licensesDir, 'foo.xml')), true, 'foo.xml file created.');
-  fs.unlinkSync(path.join(licensesDir, 'foo.xml'));
+  t.equal(fs.existsSync(path.join(util.licensesDir(), 'foo.xml')), true, 'foo.xml file created.');
+  fs.unlinkSync(path.join(util.licensesDir(), 'foo.xml'));
   t.end();
 });
 
 test('Should create HTML file.', (t) => {
   const options = {css: path.join(__dirname, '../lib/resources/licenses.css')};
-  options.directory = process.cwd();
+  options.directory = util.currentDir();
   const dependencyLicenseFiles = [];
   const foo = {
     name: 'testProject',
@@ -52,9 +52,9 @@ test('Should create HTML file.', (t) => {
   dependencyLicenseFiles.push(foo);
   return Promise.resolve(writer.createHtml(options, xmlObject, dependencyLicenseFiles))
   .then(() => {
-    t.equal(fs.existsSync(path.join(licensesDir, 'license.html')), true, 'HTML file created.');
-    fs.unlink(path.join(licensesDir, 'license.html'), () => {});
-    fs.unlink(path.join(licensesDir, 'licenses.css'), () => {});
+    t.equal(fs.existsSync(path.join(util.licensesDir(), 'license.html')), true, 'HTML file created.');
+    fs.unlink(path.join(util.licensesDir(), 'license.html'), () => {});
+    fs.unlink(path.join(util.licensesDir(), 'licenses.css'), () => {});
   });
 });
 
@@ -65,23 +65,23 @@ test('Should merge XML files.', (t) => {
   return Promise.resolve(writer.mergeXmls(options))
   .then(() => {
     t.equal(fs.access('fooBar.xml', (err) => { if (!err) return true; }, true));
-    fs.unlink(path.join(licensesDir, 'foo.xml'), () => {});
-    fs.unlink(path.join(licensesDir, 'bar.xml'), () => {});
-    setTimeout(() => fs.unlink(path.join(licensesDir, 'fooBar.xml'), () => {}), 500);
-    setTimeout(() => fs.rmdir(licensesDir, () => {}), 500);
+    fs.unlink(path.join(util.licensesDir(), 'foo.xml'), () => {});
+    fs.unlink(path.join(util.licensesDir(), 'bar.xml'), () => {});
+    setTimeout(() => fs.unlink(path.join(util.licensesDir(), 'fooBar.xml'), () => {}), 500);
+    setTimeout(() => fs.rmdir(util.licensesDir(), () => {}), 500);
   });
 });
 
 test('Should create a JSON file.', (t) => {
   writer.createJson({json: 'foo.json'}, xmlObject);
-  t.equal(fs.existsSync(path.join(licensesDir, 'foo.json')), true, 'foo.json file created.');
-  fs.unlinkSync(path.join(licensesDir, 'foo.json'));
+  t.equal(fs.existsSync(path.join(util.licensesDir(), 'foo.json')), true, 'foo.json file created.');
+  fs.unlinkSync(path.join(util.licensesDir(), 'foo.json'));
   t.end();
 });
 
 test('Should create a YAML file.', (t) => {
   writer.createYaml({yaml: 'foo.yaml'}, xmlObject);
-  t.equal(fs.existsSync(path.join(licensesDir, 'foo.yaml')), true, 'foo.yaml file created.');
-  fs.unlinkSync(path.join(licensesDir, 'foo.yaml'));
+  t.equal(fs.existsSync(path.join(util.licensesDir(), 'foo.yaml')), true, 'foo.yaml file created.');
+  fs.unlinkSync(path.join(util.licensesDir(), 'foo.yaml'));
   t.end();
 });
