d0d75903a9a2835cd96b06483578f782a5dda4f0
Helio Frota <00hf11@gmail.com>
2017-10-26 11:33:31 -0300

    src: more refactor

--- test/project-test.js
+++ test/project-test.js
@@ -2,10 +2,11 @@
 
 const test = require('tape');
 const project = require('../lib/project');
+const util = require('../lib/util');
 
 test('Checks if project has node modules.', (t) => {
   t.plan(2);
-  t.ok(project.hasNodeModules(process.cwd()), `project has node modules.`);
+  t.ok(project.hasNodeModules(util.currentDir()), `project has node modules.`);
   t.notOk(project.hasNodeModules('foo'), `project has no modules.`);
   t.end();
 });
