d74abaf47220137b6dfd3a070556c6d5c4f2003b
Gerard Ryan <gryan@redhat.com>
2017-11-22 20:32:15 +0000

    💥 Only include dev deps if option set
    This introduces an '--include-dev-dependencies' option to the console,
    report, and save commands. The default behaviour changes here also to
    include only production dependencies by default.

--- test/lib/checker-test.js
+++ test/lib/checker-test.js
@@ -4,17 +4,36 @@ const test = require('blue-tape');
 const cwd = process.cwd();
 const checker = require('../../lib/modules/checker');
 
+const numNycDeps = json => json.filter(d => d.dependency.startsWith('nyc@')).length;
+
 test('Checks licenses.', (t) => {
-  t.plan(4);
+  t.plan(5);
   return checker.check(cwd)
   .then((json) => {
     t.ok(json, `The json has content.`);
     t.ok(json[0].hasOwnProperty('dependency'), `The json has dependency property.`);
     t.ok(json[0].hasOwnProperty('licenses'), `The json has licenses property.`);
     t.ok(json[0].hasOwnProperty('file'), `The json has file property.`);
+    t.ok(numNycDeps(json) > 0, `Contains devDependency 'nyc'`);
   })
   .catch((e) => {
     console.error(e);
     t.fail(e);
   });
+});
+
+test('Checks licenses with production-only argument.', (t) => {
+  t.plan(5);
+  return checker.check(cwd, true)
+    .then((json) => {
+      t.ok(json, `The json has content.`);
+      t.ok(json[0].hasOwnProperty('dependency'), `The json has dependency property.`);
+      t.ok(json[0].hasOwnProperty('licenses'), `The json has licenses property.`);
+      t.ok(json[0].hasOwnProperty('file'), `The json has file property.`);
+      t.ok(numNycDeps(json) === 0, `Doesn't contain devDependency 'nyc'`);
+    })
+    .catch((e) => {
+      console.error(e);
+      t.fail(e);
+    });
 });
