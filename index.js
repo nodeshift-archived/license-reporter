edc58934fd938b56b270cca354e6c8f795d014c8
helio-frota <00hf11@gmail.com>
2017-12-20 19:54:49 +0000

    src: flag to hide warning messages to console command
    * src: flag to hide warning messages to console command
    * src: raw output license information

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strict';
 
 const { join } = require('path');
-const requireDir = require('require-dir');
+const consoleModule = require('./lib/modules/console.mdl');
+const canonicalName = require('./lib/utils/canonical-name');
+const unifiedList = require('./lib/utils/unified-list');
+const checker = require('./lib/utils/checker');
 
-let commands = requireDir(join(__dirname, 'lib', 'commands'));
-commands = Object.keys(commands).map((c) => commands[c]());
+/**
+ * Searches licenses of project's dependencies and send to stdout.
+ * @param {string} dir project root directory.
+ * @param {boolean} idd Include devDependencies.
+ */
+const licenses = (dir, idd) => {
+  const mappings = canonicalName.loadNameMapperFile(join(__dirname, 'lib/utils/resources/default-canonical-names.json'));
+  const canonicalNameMapper = canonicalName.init(mappings);
+  unifiedList.load(join(__dirname, 'lib/utils/resources/default-unified-list.json'));
 
-module.exports = { commands };
+  checker.check(dir, idd)
+    .then((data) => {
+      const projectMetaData = consoleModule.transform(data, canonicalNameMapper, dir, false, false, true);
+      projectMetaData.dependencies.dependency.forEach((d) => {
+        console.log(`${d.packageName} -> ${d.licenses.license[0].name}`);
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
+
+module.exports = {
+  licenses
+};
