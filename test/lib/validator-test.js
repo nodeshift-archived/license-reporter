a0db706e741a09867eb893fc9adcd1191b728863
Helio Frota <00hf11@gmail.com>
2017-12-20 11:16:17 -0300

    fix: lint

--- test/lib/validator-test.js
+++ test/lib/validator-test.js
@@ -4,7 +4,6 @@ const test = require('tape');
 const { join } = require('path');
 const stdout = require('test-console').stdout;
 const validator = require('../../lib/utils/validator');
-const cwd = process.cwd();
 
 test('Validates unified list.', (t) => {
   t.plan(1);
